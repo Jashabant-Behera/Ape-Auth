4aff8564f941860391b63c78a07b327c4a467d75
Jashabant Behera <jeetbehera143@gmail.com>
2025-03-11 19:59:36 +0530

    cmt

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ connectDB();
 
 const allowedorigins = [
 	"http://localhost:5173",
-	"https://ape-auth-1r1078jgg-jashabant-beheras-projects.vercel.app",
+	"https://ape-auth-rkv3y8czt-jashabant-beheras-projects.vercel.app",
 ];
 
 app.use(express.json());
