1e25fe78293f3fe239345a24e39f4749c44a9fdb
Jashabant Behera <jeetbehera143@gmail.com>
2025-03-14 16:08:53 +0530

    cmt

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ape-authentication.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
