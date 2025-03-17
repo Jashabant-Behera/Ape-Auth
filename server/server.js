97f7e3cd4ec3e361c4b889b02fe1fb82292c91ae
Jashabant Behera <jeetbehera143@gmail.com>
2025-03-17 20:21:02 +0530

    cmt

--- server/server.js
+++ server/server.js
@@ -11,42 +11,26 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Connect to MongoDB
 connectDB();
 
-// CORS Configuration
 const allowedOrigins = ["https://ape-auth.vercel.app", "http://localhost:5174"];
 
 app.use(
 	cors({
-		origin: (origin, callback) => {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true);
-			} else {
-				callback(new Error("Not allowed by CORS"));
-			}
-		},
-		credentials: true, // Allow cookies to be sent
+		origin: allowedOrigins,
+		credentials: true,
 	})
 );
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-// API Endpoints
 app.get("/", (req, res) => {
 	res.send("API IS WORKING");
 });
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
-app.get('/api/auth/isAuth', (req, res) => {
-    res.json({ message: 'CORS is working!' });
-});
-
-
-// Start the Server
 app.listen(port, () => {
 	console.log(`Server started on PORT: ${port}`);
 });
